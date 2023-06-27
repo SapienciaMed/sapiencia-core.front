05dfab64b17a30eff680eaa8af3a80aab78a8d97
davidhh98 <136001294+dhenao-i4digital@users.noreply.github.com>
2023-06-27 10:33:46 -0500

    cambio en validacion tk
    mostrar mensaje si el tk falla

--- src/features/home/pages/change-password.page.tsx
+++ src/features/home/pages/change-password.page.tsx
@@ -1,15 +1,16 @@
-import React from "react";
+import React, { useContext } from "react";
 
 import "../../../styles/login.scss";
 import  { useUserService }  from "../../../common/hooks/user-service.hook";
 import ChangePasswordComponent from "../components/change-password.component";
 import { EResponseCodes } from "../../../common/constants/api.enum";
 import { useNavigate } from "react-router-dom";
+import { AppContext } from "../../../common/contexts/app.context";
 
 
 function ChangePasswordRecovery(): React.JSX.Element {
   const navigate = useNavigate();
-  
+  const { setMessage } = useContext(AppContext);
   const token =   localStorage.getItem("token");  
 
   const { changePassword } = useUserService(token);
@@ -22,7 +23,16 @@ function ChangePasswordRecovery(): React.JSX.Element {
     if (operation.code === EResponseCodes.OK) {
       navigate("../login");
     } else {
-      alert("Error en el token");
+        setMessage({
+            title: "Ocurrio un error!",
+            description:
+              "El token es invalido o ha ocurrido un error inesperado, intenta nuevamente",
+            show: true,
+            OkTitle: "Aceptar",
+            onOk: () => {
+              setMessage({});
+            },
+          });
     }
   };
 
